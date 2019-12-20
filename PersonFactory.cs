9494fb43b6fa3c73d58b238f727bba853077afc1
Abhinav Pradeep <Abhinav.Pradeep@hotmail.com>
2019-12-20 11:35:58 +1000

    Renamed a few objects to make the code more readable

--- PersonFactory.cs
+++ PersonFactory.cs
@@ -1,7 +1,4 @@
 using System;
-using MongoDB;
-using MongoDB.Driver;
-using Newtonsoft.Json;
 
 namespace MongoAddress
 {
@@ -9,32 +6,32 @@ namespace MongoAddress
     {
         public Person CreatePerson()
         {
-            Person Abhinav = new Person();
+            Person person = new Person();
             System.Console.WriteLine("Enter The _id of the document");
             string personidstr = System.Console.ReadLine();
             Int32.TryParse( personidstr, out int personid);
-            Abhinav._id = personid;
+            person._id = personid;
             System.Console.WriteLine("Enter the customers first name");
-            Abhinav.FirstName = System.Console.ReadLine();
+            person.FirstName = System.Console.ReadLine();
             System.Console.WriteLine("Enter the customers last name");
-            Abhinav.LastName = System.Console.ReadLine();
+            person.LastName = System.Console.ReadLine();
 
-            Address AbhinavAddress = new Address();
+            Address address = new Address();
             System.Console.WriteLine("Enter the customers country");
-            AbhinavAddress.Country = System.Console.ReadLine();
+            address.Country = System.Console.ReadLine();
             System.Console.WriteLine("Enter the customers state");
-            AbhinavAddress.State = System.Console.ReadLine();
+            address.State = System.Console.ReadLine();
             System.Console.WriteLine("Enter the customers area");
-            AbhinavAddress.Area = System.Console.ReadLine();
+            address.Area = System.Console.ReadLine();
             System.Console.WriteLine("Enter the customers street");
-            AbhinavAddress.Street = System.Console.ReadLine();
+            address.Street = System.Console.ReadLine();
             System.Console.WriteLine("Enter the customes house number");
            Int32.TryParse( System.Console.ReadLine(), out int inthouse);
-            AbhinavAddress.HouseNum = inthouse;
+            address.HouseNum = inthouse;
 
-            Abhinav.Address = AbhinavAddress;
+            person.Address = address;
 
-            return Abhinav;
+            return person;
         }
         
         
